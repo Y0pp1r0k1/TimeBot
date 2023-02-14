ae6af3717d59e49c01880553c454c4d8187df42c
進藤仁貴 <ys08260002@outlook.jp>
2023-02-14 18:42:23 +0900

    CSSの編集

--- timeBot.js
+++ timeBot.js
@@ -4,30 +4,30 @@ let now = new Date();
 //朝の時間設定
 //現在の日時 +　午前11時59分59秒
 let morningSetTime = new Date();
-morningSetTime.setHours(11);          //何時
-morningSetTime.setMinutes(59);        //何分
-morningSetTime.setSeconds(59);        //何秒
+morningSetTime.setHours(12);          //何時
+morningSetTime.setMinutes(00);        //何分
+morningSetTime.setSeconds(00);        //何秒
 
 //昼の時間設定
 //現在の日時　+　午後5時59分59秒
 let noonSetTime = new Date();
-noonSetTime.setHours(17);
-noonSetTime.setMinutes(59);
-noonSetTime.setSeconds(59);
+noonSetTime.setHours(18);
+noonSetTime.setMinutes(00);
+noonSetTime.setSeconds(00);
 
 //夜の時間設定
 //現在の日時　+　午後23時59分59秒
 let nightSetTime = new Date();
-nightSetTime.setHours(23);
-nightSetTime.setMinutes(59);
-nightSetTime.setSeconds(59);
+nightSetTime.setHours(24);
+nightSetTime.setMinutes(00);
+nightSetTime.setSeconds(00);
 
 //深夜の時間設定
 //現在の日時　+　午前5時59分59秒
 let midnightSetTime = new Date();
-midnightSetTime.setHours(5);
-midnightSetTime.setMinutes(59);
-midnightSetTime.setSeconds(59);
+midnightSetTime.setHours(6);
+midnightSetTime.setMinutes(00);
+midnightSetTime.setSeconds(00);
 
 //時間によって返答が変わる表示の処理
 let TimeBotCode = function () {
